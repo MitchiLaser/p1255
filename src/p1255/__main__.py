87955c04bf9c375a3bd60c37b7259352c322c67b
Michael Hohenstein <michael@hohenste.in>
2025-05-15 10:27:45 +0200

    main application with cli interface available

--- src/p1255/__main__.py
+++ src/p1255/__main__.py
@@ -8,33 +8,30 @@ import ipaddress
 
 
 def main():
-    parser = argparse.ArgumentParser("P1255", description="Capture and decode data from a P1255 oscilloscope")
-    parser.add_argument(
-        "-a",
-        "--address",
-        type=ipaddress.IPv4Address,
-        help="The IPv4 address of the oscilloscope",
+    parser = argparse.ArgumentParser(
+        prog="P1255",
+        description="Capture and decode data from a P1255 oscilloscope over LAN",
+        epilog="https://github.com/MitchiLaser/p1255/"
     )
-    parser.add_argument(
-        "-p",
-        "--port",
-        type=int,
-        default=3000,
-        help="The port to connect to, default is 3000",
-    )
+    parser.add_argument("-a", "--address", type=ipaddress.IPv4Address, required=True, help="The IPv4 address of the oscilloscope", )
+    parser.add_argument("-p", "--port", type=int, default=3000, help="The port to connect to, default is 3000", )
+    parser.add_argument("-o", "--output", type=str, required=True, help="Output File where the dataset is saved", )
+    parser.add_argument("-f", "--format", type=str, choices=["csv", "json"], required=True, help="Storage file format", )
     args = parser.parse_args()
 
-    # Capture the dataset from the oscilloscope
-    print("Capturing dataset...")
     dataset = capture.capture(args.address, args.port)
-    print("Dataset captured")
-    # Decode the dataset
-    print("Decoding dataset...")
     dataset = decode.Dataset(dataset)
-    print("Dataset decoded")
 
-    # TODO: Continue
-    # TODO: This might be turned into a simple stand alone program that captures a dataset from the oscilloscope and stores it in a file (CSV, JSON, etc.)
+    with open(args.output, "w") as f:
+        if args.format == "json":
+            import json
+            data = [{"name": i.name, "timescale": i.timescale, "data": i.data} for i in dataset.channels]
+            f.write(json.dumps(data))
+        elif args.format == "csv":
+            import csv
+            writer = csv.writer(f)
+            writer.writerow([i.name for i in dataset.channels])
+            writer.writerows(zip(*[i.data for i in dataset.channels]))
 
 
 if __name__ == "__main__":
